a131940ecd7a20ecfac9f20750fa2c66c0544ecd
KITOMARI <shedrack.kitomari47@gmail.com>
2023-02-17 16:39:20 +0300

    Added post, user and subreddit models

---
+++ src/main/java/com/kitomari/RedditClone/models/User.java
@@ -0,0 +1,32 @@
+package com.kitomari.RedditClone.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import java.time.Instant;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    @NotBlank(message = "Username is required")
+    private String username;
+    @NotBlank(message = "Password is required")
+    private String password;
+    @NotBlank(message = "Email is required")
+    private String email;
+    private Instant created;
+    private boolean enabled;
+}
