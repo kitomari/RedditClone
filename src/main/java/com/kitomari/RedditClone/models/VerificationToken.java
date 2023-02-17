c0fdfb967d3dd9965aa61f7abfbc5dfa7543f98c
KITOMARI <shedrack.kitomari47@gmail.com>
2023-02-17 17:00:01 +0300

    Added Vote,VoteType,Comment and VerificationToken models

---
+++ src/main/java/com/kitomari/RedditClone/models/VerificationToken.java
@@ -0,0 +1,25 @@
+package com.kitomari.RedditClone.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Token")
+public class VerificationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+    private Instant expiryDate;
+}
---
+++ src/main/java/com/kitomari/RedditClone/models/VoteType.java
@@ -0,0 +1,10 @@
+package com.kitomari.RedditClone.models;
+
+public enum VoteType {
+    UPVOTE(1),
+    DOWNVOTE(-1);
+
+    VoteType(int direction){
+
+    }
+}
